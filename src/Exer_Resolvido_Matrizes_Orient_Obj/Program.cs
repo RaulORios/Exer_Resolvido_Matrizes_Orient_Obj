2b504adf98e1547df40bf614995f216b7bcbb981
RaulORios <raul.orios89@outlook.com>
2021-10-01 12:09:17 -0300

    Arquivos adicionados

---
+++ src/Exer_Resolvido_Matrizes_Orient_Obj/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Exer_Resolvido_Matrizes_Orient_Obj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n,n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Main diagonal:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i, i] + " ");
+            }
+
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i,j]< 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Negative numbers: " + count);
+
+            Console.ReadKey();
+
+        }
+    }
+}
